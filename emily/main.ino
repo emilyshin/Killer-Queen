3cac3c8899567d5dbf89a53bc13b1878d391dc98
Emily Shin <Emily@DN0a22952d.SUNet>
2023-02-21 14:01:59 -0800

    I added some definitions, still very basic as of right now

--- emily/main.ino
+++ emily/main.ino
@@ -1,14 +1,35 @@
 
 
-#hello
+//hello
+
+#define leftMotor 3
+#define rightMotor 5
+#define enc1 6
+#define enc2 7
+#define IR1
+#define IR2
+#define IR3
+#define IR4
+#define sideIR1
+#define sideIR2
+
 void setup(){
 
 
 
-
-
 Serial.begin(9600);
-
+pinMode(leftMotor, OUTPUT);
+pinMode(rightMotor, OUTPUT);
 
 }
 
+void loop(){
+
+
+
+
+
+
+
+
+}
